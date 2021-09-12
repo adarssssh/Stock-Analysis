4fb80ee29294b559c4aa2bfa554148a324ef5075
KD <59188880+KD1903@users.noreply.github.com>
2021-09-12 13:16:16 +0530

    Update settings.py

--- stockAnalysis/settings.py
+++ stockAnalysis/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6^@^s!z5(-c)xgve0l^51l#(b(#)_g(i5s01f5efq90sl_xj%w'
+SECRET_KEY = '6^@^s!z5(-c)***************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
